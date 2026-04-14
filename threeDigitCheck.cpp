cf606e8a2b64b2418fe4cf43fd99eb6c937071a4
Muhammad Hassaan <mrhassaan3139@gmail.com>
2026-04-14 23:07:21 +0500

    Added C++ program to check 3-digit number using AND operator

---
+++ threeDigitCheck.cpp
@@ -0,0 +1,18 @@
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+
+    cout<<"enter a number:";
+    cin>>n;
+
+    if(n>99 && n<1000)
+        cout<<"3 digit number";
+    else
+        cout<<"can not 3 digit number";
+
+}
+
